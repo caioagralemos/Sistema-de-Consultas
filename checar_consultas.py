f7faca6069559d2e1a6b2dff4004fd8b717bd1ac
caioagralemos <caioagralemos@live.com>
2023-07-07 20:53:11 -0300

    Update checar_consultas.py

--- checar_consultas.py
+++ checar_consultas.py
@@ -1,5 +1,17 @@
+import os
 import json
 import datetime
+
+diretorio = './data/'
+caminho_arquivo = f'{diretorio}reservas.json'
+
+if not os.path.isdir(diretorio):
+    os.makedirs(diretorio)
+
+if not os.path.isfile(caminho_arquivo):
+    dados_iniciais = []
+    with open(caminho_arquivo, 'w') as arquivo:
+        json.dump(dados_iniciais, arquivo)
 
 with open('./data/reservas.json', 'r') as arquivo:
     # Carrega o conteúdo do arquivo JSON em um objeto Python
@@ -14,6 +26,10 @@ def obter_data(item):
 array_filtrado = sorted(objeto_python, key=obter_data)
 
 i = 1
-for objeto in array_filtrado:
-    print(f"\nConsulta nº {i}:\nPaciente: {objeto['nome']}\nCPF: {objeto['cpf']}\nData da consulta: {objeto['data']['dia']}/{objeto['data']['mes']}/{objeto['data']['ano']}")
-    i = i + 1
+
+if array_filtrado == []:
+    print('Não temos consultas marcadas.')
+else:
+    for objeto in array_filtrado:
+        print(f"\nConsulta nº {i}:\nPaciente: {objeto['nome']}\nCPF: {objeto['cpf']}\nData da consulta: {objeto['data']['dia']}/{objeto['data']['mes']}/{objeto['data']['ano']}")
+        i = i + 1
