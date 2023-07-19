3de50bae79cdd620c4b6f5d72a821ca6d8a91870
Leandro Cavalcante <devleandromcz@gmail.com>
2023-07-19 16:50:13 -0300

    Update - New Holidays

--- components/helpers/Feriados estaduais e municipais Maceió/verificar_feriados_mcz.py
+++ components/helpers/Feriados estaduais e municipais Maceió/verificar_feriados_mcz.py
@@ -85,7 +85,7 @@ def pascoa(dia, mes, ano):
 
 def feriados_est_mcz(dia, mes, ano):
 
-    if pascoa(dia, mes, ano) or (dia == 24 and mes == 6) or (dia == 29 and mes == 6) or (dia == 16 and mes == 9) or (dia == 20 and mes == 11) or (dia == 30 and mes == 11) or (dia == 27 and mes == 8) or (dia == 8 and mes == 12):
+    if pascoa(dia, mes, ano) or (dia == 24 and mes == 6) or (dia == 29 and mes == 6) or (dia == 16 and mes == 9) or (dia == 20 and mes == 11) or (dia == 30 and mes == 11) or (dia == 27 and mes == 8) or (dia == 8 and mes == 12) or (dia == 6 and mes == 29) or (dia == 11 and mes == 20):
         return True
     else:
         return False
