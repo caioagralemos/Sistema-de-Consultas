e3070f2c1b291d2f21ccc41ad962ceda501c1788
Leandro Cavalcante <devleandromcz@gmail.com>
2023-07-19 13:56:01 -0300

    Update validacao_data.py
    Diminuindo a quantidade de inputs dentro do código.
    Como estava antes está como comentário.

--- components/helpers/validacao_data.py
+++ components/helpers/validacao_data.py
@@ -1,35 +1,37 @@
 from datetime import datetime
 
 def validacao_data():
-    ano = int(input('Diga o ano para sua consulta: '))
+    #ano = int(input('Diga o ano para sua consulta: '))
     while True:
+        ano = int(input('Diga o ano para sua consulta: '))
         if ano < datetime.now().year:
             print('Ano inválido!')
-            ano = int(input('Diga o ano para sua consulta: '))
+            #ano = int(input('Diga o ano para sua consulta: '))
         else:
             break
 
-
-    mes = int(input('Diga o mês para sua consulta: '))
+    #mes = int(input('Diga o mês para sua consulta: '))
     while True:
+        mes = int(input('Diga o mês para sua consulta: '))
         if ano == datetime.now().year and mes < datetime.now().month:
             print('Mês inválido!')
-            mes = int(input('Diga o mês para sua consulta: '))
+            #mes = int(input('Diga o mês para sua consulta: '))
         elif mes < 1 or mes > 12:
             print('Mês inválido!')
-            mes = int(input('Diga o mês para sua consulta: '))
+            #mes = int(input('Diga o mês para sua consulta: '))
         else:
             break
+        
 
-
-    dia = int(input('Diga o dia para sua consulta: '))
+    #dia = int(input('Diga o dia para sua consulta: '))
     while True:
+        dia = int(input('Diga o dia para sua consulta: '))
         if ano == datetime.now().year and mes == datetime.now().month and dia < datetime.now().day:
             print('Dia inválido!')
-            dia = int(input('Diga o dia para sua consulta: '))
+            #dia = int(input('Diga o dia para sua consulta: '))
         elif dia < 1 or dia > 31:
             print('Dia inválido!')
-            dia = int(input('Diga o dia para sua consulta: '))
+            #dia = int(input('Diga o dia para sua consulta: '))
         else:
             break
 
