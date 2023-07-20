b681fbc534a240736f5c4f9618359832255ecc34
caioagralemos <caioagralemos@live.com>
2023-07-19 22:19:48 -0300

    Update admin.py

--- _backend/consultas/admin.py
+++ _backend/consultas/admin.py
@@ -6,7 +6,7 @@ from .models import Consulta
 from .models import Consulta
 
 class ConsultaAdmin(admin.ModelAdmin):
-    list_display = ('paciente', 'medico', 'data') # Elementos a serem mostrados na lista
+    list_display = ('paciente', 'medico', 'data', 'servico') # Elementos a serem mostrados na lista
     list_display_links = ('paciente',  'medico') # Elementos que quando clicados levam a página de edição
     search_fields = ('paciente',  'medico', 'data') # itens que podem ser pesquisados
     list_per_page = 25 # quantos elementos por página
