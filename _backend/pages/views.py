74c72c759d2ba016632dde4fcdb4ff5fdaedc74d
caioagralemos <caioagralemos@live.com>
2023-07-25 20:16:18 -0300

    Página de Marcação de Consultas

--- _backend/pages/views.py
+++ _backend/pages/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from medico.models import Medico
+
 # Create your views here.
 
 def index(request):
     return render(request, 'pages/index.html')
 
 def agendar_consulta(request):
-    return render(request, 'pages/marcar_consulta.html')
-
+    context = {
+        'medicos': Medico.objects.all(),
+    }
+    return render(request, 'pages/marcar_consulta.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
