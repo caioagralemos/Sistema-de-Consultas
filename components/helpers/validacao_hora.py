abe302e00a28f9d226a2027f6c473d4915f9e64f
Leandro Cavalcante <devleandromcz@gmail.com>
2023-08-01 10:02:08 -0300

    Update validacao_hora.py

--- components/helpers/validacao_hora.py
+++ components/helpers/validacao_hora.py
@@ -1,28 +1,27 @@
 from components.helpers.verificacao_HoraeData import comparar_horario
 
 def validacao_hora(dia, mes, ano):
+    hora = [0]
+    minuto = [0]
     while True:
         op = int(input('Diga o período para sua consulta:\n1 para manhã\n2 para tarde\n'))
         if op == 1:
             while True:
-                hora = []
-                hora.append(int(input('Diga a hora para sua consulta: ')))
+                hora[0] = int(input('Diga a hora para sua consulta: '))
                 if hora[0] > 5 and hora[0] < 13:
                     break
                 else:
                     print('Hora inválida!')
         else:
             while True:
-                hora = []
-                hora.append(int(input('Diga a hora para sua consulta: ')))
-                if hora[0] > 13 and hora[0] < 19:
+                hora[0] = int(input('Diga a hora para sua consulta: '))
+                if hora[0] > 14 and hora[0] < 20:
                     break
                 else:
                     print('Hora inválida!')
 
         while True:
-            minuto = []
-            minuto.append(int(input('Diga o minuto para sua consulta: ')))
+            minuto[0] = int(input('Diga o minuto para sua consulta: '))
             if minuto[0] == 0 or minuto[0] == 30:
                 break
             elif minuto[0] >= 0 and minuto[0] <= 59:
